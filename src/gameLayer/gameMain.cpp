b3679d367a731e635847d4420a9445ddc5bc782a
TheOnlyBlake <blake.blumenfeld@gmail.com>
2026-04-13 12:54:39 -0400

    Asset Manager and Game Map Added an asset manager to handle all textures Added the gameMap class to handle the creation of the world and have checks for the bounds of the map

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,15 +1,29 @@
 #include <raylib.h>
 #include "gameMain.h"
 #include <asserts.h>
+#include <assetManager.h>
+#include <gameMap.h>
 
 struct GameData {
 
-    float positionX = 100;
-    float positionY = 100;
+    GameMap gameMap;
 
 }gameData;
 
+AssetManager assetManager;
+
 bool initGame() {
+
+    assetManager.loadAll();
+
+    gameData.gameMap.create(30, 10);
+
+    gameData.gameMap.getBlockUnsafe(0, 0).type = Block::dirt;
+    gameData.gameMap.getBlockUnsafe(1, 1).type = Block::dirt;
+    gameData.gameMap.getBlockUnsafe(2, 2).type = Block::dirt;
+    gameData.gameMap.getBlockUnsafe(3, 3).type = Block::dirt;
+    gameData.gameMap.getBlockUnsafe(4, 4).type = Block::dirt;
+
     return true;
 }
 
@@ -18,12 +32,28 @@ bool updateGame() {
     float deltaTime = GetFrameTime();
     if (deltaTime > 1.f / 5) { deltaTime = 1 / 5.f; }
 
-    if (IsKeyDown(KEY_A)) { gameData.positionX -= 200 * deltaTime; }
-    if (IsKeyDown(KEY_D)) { gameData.positionX += 200 * deltaTime; }
-    if (IsKeyDown(KEY_W)) { gameData.positionY -= 200 * deltaTime; }
-    if (IsKeyDown(KEY_S)) { gameData.positionY += 200 * deltaTime; }
+    ClearBackground({75, 75, 150, 255});
 
-    DrawRectangle(gameData.positionX, gameData.positionY, 50, 50, {255, 0, 200, 255});
+    for (int y = 0; y < gameData.gameMap.h; y++)
+        for (int x = 0; x < gameData.gameMap.w; x++) {
+
+            auto &b = gameData.gameMap.getBlockUnsafe(x, y);
+
+            if (b.type != Block::air) {
+                float size = 32;
+                float posX = x * size;
+                float posY = y * size;
+
+                DrawTexturePro(
+                    assetManager.dirt,
+                    Rectangle{0.f, 0.f, (float)assetManager.dirt.width, (float)assetManager.dirt.height}, //source
+                    {posX, posY, size, size}, // dest
+                    {0, 0}, //origin (top-left corner)
+                    0.0f, //rotation
+                    WHITE // tint
+                    );
+            }
+        }
 
     return true;
 }
